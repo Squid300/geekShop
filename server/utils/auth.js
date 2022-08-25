0b39a19d4c2cea5443a1f6dd11c1bc975ab93d34
Squid300 <squid400@gmail.com>
2022-08-25 18:38:46 -0400

    update connection.js and resolvers

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,11 @@
 const jwt = require('jsonwebtoken');
-
 const secret = 'mysecretsshhhhh';
 const expiration = '24h';
 
 module.exports = {
   authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }

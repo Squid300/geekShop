7795aa75d192271c20a6f5a579eb75ed68d92861
mendelism <hgustafson17@gmail.com>
2022-08-24 21:24:07 -0400

    navigation menu, some file cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,11 @@ import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Nav from './components/Nav';
+import Navi from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import 'material-symbols';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -37,13 +38,14 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
         <div>
           <StoreProvider>
-            <Nav />
+            <Navi />
             <Routes>
               <Route 
                 path="/" 
